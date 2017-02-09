897b4dc15954383dc046a5714b8d96242913caaa
Davide Cavaliere <cavaliere.davide@gmail.com>
2017-02-09 16:03:22 +0100

    adding user model and user-form

---
+++ src/app/models/user-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+// import { User } from './user';
+
+// @Component({
+//   moduleId : module.id,
+//   selector : 'login-form',
+//   templateUrl : './user-form.component.html'
+// })
+// export class UserFormComponent {
+//   model = new User('a4f53b', 'test', 'test@email.it', 'guest');
+//   // submitted
+// }
