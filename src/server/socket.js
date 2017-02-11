6304e8a1be3348e9445460a56cfe81f12c7b8fa9
Davide Cavaliere <cavaliere.davide@gmail.com>
2017-02-11 11:10:19 +0100

    adding listeners on socket server

--- src/server/socket.js
+++ src/server/socket.js
@@ -1,8 +1,40 @@
 var server = require('http').createServer();
 
+function* itemsGenerator(n = 0) {
+
+  let item = {
+    title : `title ${n}`,
+    date : new Date(),
+    text  : `body ${n}`,
+  }
+
+  if (n === 0) return item;
+
+  yield item;
+  yield* itemsGenerator(--n);
+
+}
+
 var io = require('socket.io')(server);
-io.on('connection', client => {
-  console.log('client');
+io.on('connection', socket => {
+  console.log('client conneted: ', socket.id);
+
+
+  socket.on('stories:list', () => {
+    console.log('got stories:list event');
+    let items = [];
+    let itemsFactory = itemsGenerator(5);
+    let i = itemsFactory.next();
+
+    while (!i.done) {
+
+      items.push(i.value);
+      i = itemsFactory.next();
+    }
+    console.log('stories fetched', items);
+
+    socket.emit('stories:listed', items);
+  });
 });
 
 server.listen(3003);
